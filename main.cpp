565c3e3877d8be291fa103485a2774b5197a4122
Dillon Swanson <dillon_kswanson@yahoo.com>
2014-12-04 15:27:42 -0700

    Updated window locations

--- main.cpp
+++ main.cpp
@@ -11,14 +11,16 @@
 // This function will either erase or print a block on the screen. Bool set
 // to false for erase and true for print. The y and x coordinated are
 // where the erasing or printing should be done in the window.
-void paint(bool draw, int y, int x);
+void paint(WINDOW *win, bool draw, int y, int x);
 
 int main() {
+    int x1, x2, x3, y1, y2, y3;
+
     initscr();
-    WINDOW * gameWin = newwin(22, 35, 5, 5);  // creates a new window 
+    WINDOW * gameWin = newwin(22, 35, 5, 2);  // creates a new window 
     					     // params are (nlines, ncols, ystart, xstart)
-    WINDOW * nextBlockWin = newwin(7, 14, 5, 41);
-    WINDOW * infoWin = newwin(12, 14, 13, 41);
+    WINDOW * nextBlockWin = newwin(7, 14, 5, 38);
+    WINDOW * infoWin = newwin(12, 14, 13, 38);
 
     if (has_colors() == FALSE) {
         endwin();
@@ -36,16 +38,21 @@ int main() {
     wborder(gameWin, 0, 0, 0, 0, 0, 0, 0, 0);  // creates the border around the specified window
     wborder(nextBlockWin, 0, 0, 0, 0, 0, 0, 0, 0);
     wborder(infoWin, 0, 0, 0, 0, 0, 0, 0, 0);
+    
     refresh();
     wrefresh(gameWin); // needed to refresh a specific window
     wrefresh(nextBlockWin);
     wrefresh(infoWin);
     
+    getmaxyx(gameWin, y1, x1);
+
+//    mvprintw(22, 45, "Size is x: %d, y: %d",x1, y1); 
+
     int c = 0; // Character read from stdin
     int x = 0; // x coordinate (column)
     int y = 0; // y coordinate (row)
     do { // do-while loop, may be replaced for a while loop
-        paint(false, y, x); // erase the previous block
+        paint(gameWin, false, y, x); // erase the previous block
         switch(c) {
             case KEY_RIGHT:
                 x += 2; // One block unit is actually 2 space characters.
@@ -57,14 +64,14 @@ int main() {
                 y++;  // is that ANY input will suspend this.
                 break;
         }
-        paint(true, y, x); // draw the block in the new position
+        paint(gameWin, true, y, x); // draw the block in the new position
     } while ((c = getch()));
 
     endwin();
     return 0;
 }
 
-void paint(bool draw, int y, int x) {
+void paint(WINDOW *win, bool draw, int y, int x) {
     init_pair(1, COLOR_RED, COLOR_BLACK);
     init_pair(2, COLOR_BLACK, COLOR_BLACK);
     int pair = 1; // Default red
@@ -72,11 +79,14 @@ void paint(bool draw, int y, int x) {
         pair = 2; // Change to black
     // Remember that a square in the terminal is actually 2 spaces
     attron(COLOR_PAIR(pair));
+//    wmove(win, y, x);
     move(y, x);
     addch(' '|A_REVERSE); // draw first space
+//    wmove(win, y, x + 1);
     move(y, x + 1);
     addch(' '|A_REVERSE); // draw second space
     attroff(COLOR_PAIR(pair));
     refresh();
+//    wrefresh(win);
 }
 
